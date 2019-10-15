70a58c5665f5c536cdded6692a4e513cfbeeb9aa
Eduardo Gaite <1033629@ucc.edu.ar>
2019-10-15 10:27:06 -0300

    Initial Commit

---
+++ src/main/java/ar/edu/ucc/arqsoft/test/model/ObjetoGenerico.java
@@ -0,0 +1,23 @@
+package ar.edu.ucc.arqsoft.test.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class ObjetoGenerico {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
